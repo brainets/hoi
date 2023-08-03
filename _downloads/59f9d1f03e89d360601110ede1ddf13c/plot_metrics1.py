5623abb3c552ef8009ddd802e26cddf44e5d03c5
EtienneCmb <e.combrisson@gmail.com>
2023-08-03 10:58:57 +0000

    Deploying to gh-pages from @ brainets/hoi@659268e436f5c8729dd6347189856c9b1a5dd531 🚀

--- _downloads/59f9d1f03e89d360601110ede1ddf13c/plot_metrics1.py
+++ _downloads/59f9d1f03e89d360601110ede1ddf13c/plot_metrics1.py
@@ -1,7 +1,7 @@
 """
 Metrics example 1
 ============================================
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt neque, perferendis sed id doloribus at error in ea nulla reprehenderit optio rerum nostrum harum voluptas, sequi laudantium numquam accusantium consequuntur minus quisquam autem magnam distinctio. Itaque, fuga quisquam. Dolorum commodi molestiae soluta, dolorem tenetur magni illum distinctio minus vero tempore quibusdam totam sed, sapiente aliquam aperiam sunt accusantium quisquam eos rerum cupiditate fuga debitis inventore. Exercitationem nihil ipsa aliquid, placeat laborum ut vel odio deserunt adipisci minima blanditiis aperiam nisi veniam cum labore deleniti reiciendis earum eligendi dignissimos suscipit beatae ex a! Omnis commodi modi magnam esse molestias odit, non unde eius vel, aspernatur perferendis voluptatibus nam nihil, aut et. Molestias, quos quia soluta minus illum amet cupiditate iure a assumenda debitis sint dolor, dolorem at. Tempora, dolorum maiores! Explicabo molestias nostrum nihil non laboriosam accusantium aut maxime. Tenetur similique natus quos accusamus, dicta iure? Exercitationem id aut laborum itaque consequatur delectus molestias labore sequi. Libero distinctio laboriosam debitis possimus beatae nisi quae autem in voluptatibus labore vero error omnis neque, quisquam culpa odio! Tempore neque ipsa voluptatibus alias laudantium id voluptates reiciendis dolorem iusto cumque tenetur illum aliquid maiores totam sint rem repellat, repellendus molestias deserunt deleniti odit mollitia?
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
 """
 import numpy as np
 
@@ -10,7 +10,7 @@ from hoi.plot import plot_landscape
 
 import matplotlib.pyplot as plt
 
-plt.style.use('ggplot')
+plt.style.use("ggplot")
 
 x = np.random.rand(500, 10)
 # x[:, 7] = x[:, 0] + x[:, 1]
@@ -19,9 +19,8 @@ x = np.random.rand(500, 10)
 
 
 model = Oinfo(x)
-hoi = model.fit(minsize=3, maxsize=None, method='gcmi')
+hoi = model.fit(minsize=3, maxsize=None, method="gcmi")
 
-plot_landscape(hoi, model=model,
-               plt_kwargs=dict(cmap='turbo'))
+plot_landscape(hoi, model=model, plt_kwargs=dict(cmap="turbo"))
 plt.tight_layout()
 plt.show()

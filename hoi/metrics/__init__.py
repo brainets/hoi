01bca60dcad671954151c24bc0d39d885495c6c6
Pierre Robiglio <83019028+thomasrobiglio@users.noreply.github.com>
2023-10-17 17:57:25 +0200

    fix : added new metrics to __init__.py

--- hoi/metrics/__init__.py
+++ hoi/metrics/__init__.py
@@ -2,6 +2,9 @@ from .gradient_oinfo import GradientOinfo  # noqa
 from .infotopo import InfoTopo  # noqa
 from .info_tot import InfoTot  # noqa
 from .oinfo import Oinfo  # noqa
+from .sinfo import Sinfo  # noqa
+from .tc import TC  # noqa
+from .dtc import DTC  # noqa
 from .rsi import RSI  # noqa
 from .red_mmi import RedundancyMMI  # noqa
 from .syn_mmi import SynergyMMI  # noqa

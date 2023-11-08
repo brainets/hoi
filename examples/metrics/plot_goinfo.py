1e924da199085ae81d994b91b1efcc0f285b5023
Mattehub <matteblacks98@gmail.com>
2023-11-08 23:34:53 +0100

    adding a first version of example about gradient oinfo

--- examples/metrics/plot_goinfo.py
+++ examples/metrics/plot_goinfo.py
@@ -1,3 +1,21 @@
+"""
+Gradient Oinfo
+==============
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt neque,
+perferendis sed id doloribus at error in ea nulla reprehenderit optio rerum
+nostrum harum voluptas, sequi laudantium numquam accusantium consequuntur minus
+quisquam autem magnam distinctio. Itaque, fuga quisquam. Dolorum commodi
+molestiae soluta, dolorem tenetur magni illum distinctio minus vero tempore
+quibusdam totam sed, sapiente aliquam aperiam sunt accusantium quisquam eos
+rerum cupiditate fuga debitis inventore. Exercitationem nihil ipsa aliquid,
+placeat laborum ut vel odio deserunt adipisci minima blanditiis aperiam nisi
+veniam cum labore deleniti reiciendis earum eligendi dignissimos suscipit
+beatae ex a! Omnis commodi modi magnam esse molestias odit, non unde eius vel,
+aspernatur perferendis voluptatibus nam nihil, aut et. Molestias, quos quia
+soluta minus illum amet cupiditate iure a assumenda debitis sint dolor, dolorem
+at.
+"""
+
 import numpy as np
 from hoi.utils import get_nbest_mult
 from hoi.metrics import GradientOinfo

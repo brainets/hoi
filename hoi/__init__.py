aa26a46d8184089982ed1efa5cd7393d74e21fa0
Mattehub <matteblacks98@gmail.com>
2026-01-19 16:37:38 +0000

    preparing for new release, version = 0.0.6

--- hoi/__init__.py
+++ hoi/__init__.py
@@ -1,3 +1,3 @@
 from hoi import core, metrics, simulation, utils  # noqa
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 from setuptools import find_packages, setup
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 NAME = "hoi"
 AUTHOR = "BraiNets"
 MAINTAINER = "Etienne Combrisson"
